1be09966d1accb280c0236f19c4788da7492c1e6
Shiaba <>
2023-12-22 23:01:57 +0000

    Add image_filter and image validation

--- profiles/views.py
+++ profiles/views.py
@@ -8,11 +8,6 @@ from drf_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
-    """
-    List all profiles
-    No Create view (post method), as profile creation handled by django signals
-    """
-
     def get(self, request):
         profiles = Profile.objects.all()
         serializer = ProfileSerializer(
